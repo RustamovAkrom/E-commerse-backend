b3b83fdfc836ae52d992dfae5d274c6b4d5a6261
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2024-09-08 21:45:27 +0500

    Create abstract models

--- apps/shared/models.py
+++ apps/shared/models.py
@@ -1,1 +1,16 @@
 from django.db import models  # noqa
+
+
+class TimestempedAbstractModel(models.Model):
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class SlugstempedAbstractModel(models.Model):
+    slug = models.CharField(max_length=200)
+
+    class Meta:
+        abstract = True
