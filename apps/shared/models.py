f672a49c7b14e228d1fd011b7de6aae5c5d56215
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2024-09-08 22:19:07 +0500

    fixing styling all pages black

--- apps/shared/models.py
+++ apps/shared/models.py
@@ -4,25 +4,16 @@ from django.db import models  # noqa
 
 class TimestempedAbstractModel(models.Model):
     created_at = models.DateTimeField(
-        verbose_name=_("Created at"),
-        auto_now_add=True,
-        editable=False
+        verbose_name=_("Created at"), auto_now_add=True, editable=False
     )
-    updated_at = models.DateTimeField(
-        verbose_name=_("Updated at"),
-        auto_now=True
-    )
+    updated_at = models.DateTimeField(verbose_name=_("Updated at"), auto_now=True)
 
     class Meta:
         abstract = True
 
 
 class SlugstempedAbstractModel(models.Model):
-    slug = models.CharField(
-        verbose_name=_("URL"),
-        max_length=255,
-        unique=True
-    )
+    slug = models.CharField(verbose_name=_("URL"), max_length=255, unique=True)
 
     class Meta:
         abstract = True
