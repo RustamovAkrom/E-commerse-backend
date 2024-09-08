c8b4b77cef8ccf35f61907091a6b7a0e2aabb6e6
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2024-09-08 22:35:38 +0500

    creating store models

--- apps/store/models.py
+++ apps/store/models.py
@@ -1,1 +1,58 @@
+from django.conf import settings
+from django.utils.translation import gettext_lazy as _
 from django.db import models  # noqa
+from apps.shared.models import SlugstempedAbstractModel, TimestempedAbstractModel
+
+
+class ProductManager(models.Manager):
+    def get_queryset(self):
+        return super(ProductManager, self).get_queryset().filter(is_active=True)
+
+
+class Category(TimestempedAbstractModel, SlugstempedAbstractModel):
+    """
+    Category Table implimented with MPTT.
+    """
+
+    name = models.CharField(
+        verbose_name=_("Category Name"),
+        help_text=_("Required and unique"),
+        max_length=255,
+        unique=True,
+    )
+    is_active = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = _("Category")
+        verbose_name_plural = _("Categories")
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Product(TimestempedAbstractModel, SlugstempedAbstractModel):
+    category = models.ForeignKey(
+        Category, related_name="product", on_delete=models.CASCADE
+    )
+    created_by = models.ForeignKey(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE,
+        related_name="product_creator",
+    )
+    title = models.CharField(max_length=255)
+    author = models.CharField(max_length=255, default="admin")
+    description = models.TextField(blank=True)
+    image = models.ImageField(upload_to="images/", default="images/default.png")
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_stock = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=True)
+    objects = models.Manager()
+    products = ProductManager()
+
+    class Meta:
+        verbose_name = _("Product")
+        verbose_name_plural = _("Products")
+        ordering = ("-created_at",)
+
+    def __str__(self) -> str:
+        return self.title
