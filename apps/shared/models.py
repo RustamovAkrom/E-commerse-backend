480c0949922c4f2af0ccf273d8edb020ea562f05
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2024-09-08 22:14:55 +0500

    update abstract models

--- apps/shared/models.py
+++ apps/shared/models.py
@@ -1,16 +1,28 @@
+from django.utils.translation import gettext_lazy as _
 from django.db import models  # noqa
 
 
 class TimestempedAbstractModel(models.Model):
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
+    created_at = models.DateTimeField(
+        verbose_name=_("Created at"),
+        auto_now_add=True,
+        editable=False
+    )
+    updated_at = models.DateTimeField(
+        verbose_name=_("Updated at"),
+        auto_now=True
+    )
 
     class Meta:
         abstract = True
 
 
 class SlugstempedAbstractModel(models.Model):
-    slug = models.CharField(max_length=200)
+    slug = models.CharField(
+        verbose_name=_("URL"),
+        max_length=255, 
+        unique=True
+    )
 
     class Meta:
         abstract = True
