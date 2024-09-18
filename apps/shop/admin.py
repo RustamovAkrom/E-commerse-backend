7c479a71155eb5a861ddc11665a91536095161b6
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2024-09-18 14:25:51 +0500

    finished shop py files

--- apps/shop/admin.py
+++ apps/shop/admin.py
@@ -1,1 +1,26 @@
-from django.contrib import admin  # noqa
+from django.contrib import admin
+from .models import Product, Category
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = [
+        'title',
+        'author',
+        'price',
+        'is_stock',
+        'is_active',
+        'created_at',
+        'updated_at',
+    ]
+    list_filter = ['in_stock', 'is_active']
+    list_editable = ['price', 'in_stock']
+    prepopulated_fields = {'slug': ('title', )}
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug']
+    prepopulated_fields = {'slug': ('name', )}
+
+
--- apps/shop/views.py
+++ apps/shop/views.py
@@ -4,15 +4,17 @@ from .models import Category, Product
 
 def product_all(request):
     products = Product.products.all()
-    return render(request, 'shop/index.html', {'products': products})
+    return render(request, "shop/index.html", {"products": products})
 
 
 def category_link(request, category_slug=None):
     category = get_object_or_404(Category, slug=category_slug)
     products = Product.products.filter(category=category)
-    return render(request, 'shop/category.html', {'category': category, 'products': products})
+    return render(
+        request, "shop/category.html", {"category": category, "products": products}
+    )
 
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug, in_stock=True)
-    return render(request, 'shop/single.html', {'product': product})
+    return render(request, "shop/single.html", {"product": product})
