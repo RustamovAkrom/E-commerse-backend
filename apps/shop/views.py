76749f32e548132eceb597c58032ae61e46ea349
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2024-09-18 14:07:31 +0500

    create view functions on shop

--- apps/shop/views.py
+++ apps/shop/views.py
@@ -1,1 +1,18 @@
-from django.shortcuts import render  # noqa
+from django.shortcuts import render, get_object_or_404
+from .models import Category, Product
+
+
+def product_all(request):
+    products = Product.products.all()
+    return render(request, 'shop/index.html', {'products': products})
+
+
+def category_link(request, category_slug=None):
+    category = get_object_or_404(Category, slug=category_slug)
+    products = Product.products.filter(category=category)
+    return render(request, 'shop/category.html', {'category': category, 'products': products})
+
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug, in_stock=True)
+    return render(request, 'shop/single.html', {'product': product})
