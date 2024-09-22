31b85995e5bbaeb3b6b12d62e72079416f096d26
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2024-09-22 20:58:04 +0500

    🟡🔴: Update and Delete fayles

--- apps/account/views.py
+++ apps/account/views.py
@@ -1,6 +1,2 @@
-from django.contrib.auth import authenticate, login, logout
-from django.shortcuts import render
-
-
-def login(request):
-    pass
+from django.contrib.auth import authenticate, login, logout # noqa
+from django.shortcuts import render # noqa
