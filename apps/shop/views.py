443d910c574fca052577e9f0868c82091a0deb41
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2024-09-26 15:21:12 +0500

    create added on cart form and update product_detail function

--- apps/shop/views.py
+++ apps/shop/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from apps.cart.forms import CartAddProductForm
 from .models import Category, Product
 
 
@@ -7,22 +8,35 @@ def home(request):
     return render(request, "shop/index.html", {"products": products})
 
 
-def product_all(request):
+def product_list(request, category_slug=None):
+    category = None
+    categories = Category.categories.all()
     products = Product.products.all()
-    return render(request, "shop/shop.html", {"products": products})
-
-
-def category_link(request, category_slug=None):
-    category = get_object_or_404(Category, slug=category_slug)
-    products = Product.products.filter(category=category)
+    if category_slug:
+        category = get_object_or_404(Category, slug=category_slug)
+        products = products.filter(category=category)
     return render(
-        request, "shop/category.html", {"category": category, "products": products}
+        request, 
+        "shop/shop.html", 
+        {
+            "products": products,
+            "categories": categories,
+            "category": category
+        }
     )
 
 
 def product_detail(request, slug):
     product = get_object_or_404(Product, slug=slug, in_stock=True)
-    return render(request, "shop/single.html", {"product": product})
+    cart_product_form = CartAddProductForm()
+    return render(
+        request, 
+        "shop/single.html", 
+        {
+            "product": product,
+            "cart_product_form": cart_product_form
+        }
+    )
 
 
 def about(request):
