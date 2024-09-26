ad4e245531f5dcf5e3d32db088ffbfc329fbc667
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2024-09-26 14:25:57 +0500

    create Coupon model

--- apps/coupons/models.py
+++ apps/coupons/models.py
@@ -1,3 +1,17 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.utils.translation import gettext_lazy as _
 from django.db import models
 
-# Create your models here.
+
+class Coupon(models.Model):
+    code = models.CharField(max_length=50, unique=True)
+    valid_from = models.DateTimeField()
+    valid_to = models.DateTimeField()
+    discount = models.IntegerField(
+        validators=[MinValueValidator(0), MaxValueValidator(100)],
+        help_text=_("Percentage value (0, 100)"),
+    )
+    active = models.BooleanField()
+
+    def __str__(self) -> str:
+        return self.code
