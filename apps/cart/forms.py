f5f03d22b06757f373290be89199f4636b014cc8
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2024-09-26 21:31:27 +0500

    change errors

--- apps/cart/forms.py
+++ apps/cart/forms.py
@@ -2,7 +2,7 @@ from django.utils.translation import gettext_lazy as _
 from django import forms
 
 
-PRODUCT_QUANTITY_CHOICES = [(1, str(i) for i in range(1, 21))]
+PRODUCT_QUANTITY_CHOICES = [(i, str(i)) for i in range(1, 21)]
 
 
 class CartAddProductForm(forms.Form):
--- apps/shop/urls.py
+++ apps/shop/urls.py
@@ -6,9 +6,9 @@ app_name = "shop"
 
 urlpatterns = [
     path("", views.home, name="home"),
-    path("products/", views.product_all, name="products"),
+    path("products/", views.product_list, name="products"),
     path("product/<slug:slug>", views.product_detail, name="product_detail"),
-    path("shop/<slug:category_slug>", views.category_link, name="category_list"),
+    path("shop/<slug:category_slug>", views.product_list, name="category_list"),
     path("about/", views.about, name="about"),
     path("contact/", views.contact, name="contact"),
 ]
