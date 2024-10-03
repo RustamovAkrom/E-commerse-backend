0359b2ef11ab7fb021aa0ccb5ad3dec9244c8468
Akromjon <152626511+RustamovAkrom@users.noreply.github.com>
2024-10-03 13:43:15 +0500

    delete checkout app

--- apps/checkout/choices.py
+++
@@ -1,7 +0,0 @@
-from django.db import models
-
-
-class DeliveryChoices(models.TextChoices):
-    IN_STORE = "is", "In Store"
-    HOME_DELIVERY = "hd", "Home Delivery"
-    DIGITAL_DELIVERY = "dd", "Digital Delivery"
